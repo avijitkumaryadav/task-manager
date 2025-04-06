b45321b27335541691a546ea20e14395533747ac
avijitkumaryadav <kumaravijit010@gmail.com>
2025-04-06 14:22:42 +0530

    Creating Recent Tasks Section

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -9,6 +9,8 @@ import { API_PATHS } from "../../utils/apiPaths";
 import moment from "moment";
 import { addThousandsSeparator } from "../../utils/helper";
 import InfoCard from "../../components/Cards/InfoCard";
+import { LuArrowRight } from "react-icons/lu";
+import TaskListTable from "../../components/TaskListTable";
 
 const Dashboard = () => {
   useUserAuth();
@@ -33,6 +35,10 @@ const Dashboard = () => {
       console.error("Error fetching users:", error);
     }
   };
+
+  const onSeeMore = ()=>{
+    navigate('/admin/tasks')
+  }
   
   useEffect(() => {
     getDashboardData();
@@ -84,6 +90,22 @@ const Dashboard = () => {
             )}
             color="bg-lime-500"
           />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
+        <div className="md:col-span-2">
+          <div className="card">
+            <div className="flex items-center justify-between ">
+              <h5 className="text-lg">Recent Tasks</h5>
+
+              <button className="card-btn" onClick={onSeeMore}>
+                See All <LuArrowRight className="text-base" />
+              </button>
+            </div>
+
+            <TaskListTable tableData={dashboardData?.recentTasks || []} />
+          </div>
         </div>
       </div>
     </DashboardLayout>
